26e48dde576cbe5c35b4bd2712159e30b4e67f62
Kakha Putkaradze <kakha.putkaradze@eu.edu.ge>
2024-09-18 15:36:28 +0400

    test

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
 import './bootstrap';
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAdTp_M_vB8L2Y5QFMvcQWKZPqUyBsm1Io",
+  authDomain: "redberry-kp.firebaseapp.com",
+  projectId: "redberry-kp",
+  storageBucket: "redberry-kp.appspot.com",
+  messagingSenderId: "102077587892",
+  appId: "1:102077587892:web:acc0216caa74d9731e12e1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
